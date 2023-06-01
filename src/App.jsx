40b8c09bbc2a0c9bc3b4204e7a9c44cd230fb448
The-little-spring <100764083+The-little-spring@users.noreply.github.com>
2023-06-01 12:53:23 +0330

    make OurTeam component responsive

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const { darkMode } = useContext(ThemeContext);
   return (
     <div
-      className={`${
+      className={`min-vh-100 ${
         darkMode ? "bg-dark text-light" : "bg-secondary text-dark"
       }`}
     >
--- src/Components/OurTeam/ourTeam.jsx
+++ src/Components/OurTeam/ourTeam.jsx
@@ -4,41 +4,38 @@ import Pagination from "../Pagination/pagination";
 import Card from "../Card/card";
 
 const pageSize = 3;
+
 function OurTeam() {
   const [page, setPage] = useState(1);
   const [isLoading, programmers] = useFetchedData({ page, pageSize });
 
   return (
-    <div
-      className="d-flex row align-items-center justify-content-center mx-5"
-      style={{ height: "100vh" }}
-      dir="rtl"
-    >
-      {isLoading && (
-        <div className="d-flex">
-          <div className="spiner-border"></div>
-        </div>
-      )}
-      {!isLoading && (
-        <>
+    <div className="container mt-5">
+      <div className="row align-items-center justify-content-center">
+        {isLoading && (
           <div className="d-flex">
-            {programmers.data.map(({ id, ...programmer }) => {
-              return (
-                <div className="col-4" key={id}>
-                  <Card {...programmer} />
-                </div>
-              );
-            })}
+            <div className="spinner-border"></div>
           </div>
-          <div className="row mt-n5">
-            <Pagination
-              pages={Math.ceil(programmers.totalRecords / pageSize)}
-              setPage={setPage}
-              page={page}
-            />
-          </div>
-        </>
-      )}
+        )}
+        {!isLoading && (
+          <>
+            {programmers.data.map(({ id, ...programmer }) => (
+              <div className="col-md-4 col-xl-4 mb-4" key={id}>
+                <Card {...programmer} />
+              </div>
+            ))}
+            <div className="row mt-5">
+              <div className="col">
+                <Pagination
+                  pages={Math.ceil(programmers.totalRecords / pageSize)}
+                  setPage={setPage}
+                  page={page}
+                />
+              </div>
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 }
